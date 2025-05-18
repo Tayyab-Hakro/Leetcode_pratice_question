36f15eda27c7e65b775561a34960239fb7810de8
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-18 06:04:42 -0700

    question solved

--- Q1.js
+++ Q1.js
@@ -4,9 +4,11 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let res = []
-    for(let i = 0 ; i<word1.length ;i++){
-  res =   [word1[i] + word1[i+1]]= [word2[i] + word2[i+1]]
+    let res = ""
+    let maxlength = Math.max(word1.length , word2.length)
+    for(let i = 0 ; i<maxlength ;i++){
+        if( i < word1.length) res+=word1[i]
+        if( i < word2.length) res+=word2[i]
     }
     return res
 };
