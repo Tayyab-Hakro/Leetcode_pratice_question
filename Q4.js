c570898bcb25c92d688440f7eadefab1b2ba2740
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-20 02:49:34 -0700

    working

--- Q4.js
+++ Q4.js
@@ -1,8 +1,15 @@
-function Uptodo(num){
-    let i = num;
-    while(i > 0){
-        console.log(i--);
-    }
+function* fibonacci() {
+  let a = 0;
+  let b = 1;
+  
+  while (true) {
+    yield a;         // Yield the current Fibonacci number
+    [a, b] = [b, a + b];  // Update a and b to the next numbers in the sequence
+  }
 }
 
-Uptodo(5);
+// Example: print first 10 Fibonacci numbers
+const gen = fibonacci();
+for (let i = 0; i < 10; i++) {
+  console.log(gen.next().value);
+}
