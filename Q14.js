6acd24236264c8228184ecdb62ba882724c49b3d
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-23 21:28:12 +0500

    day completed

---
+++ Q14.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} hours
+ * @return {number}
+ */
+var countCompleteDayPairs = function(hours) {
+let count = 0
+for(let i = 0 ; i<hours.length ;i++){
+for(let j = i+1 ; j<hours.length ;j++){
+    if((hours[i] + hours[j]) % 24 === 0 ){
+        count++
+    }
+}
+}
+return count
+};
+console.log(countCompleteDayPairs([12,12,30,24,24]))
