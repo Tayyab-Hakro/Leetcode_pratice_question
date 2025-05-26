386a734366de39564127b9888e323a3cc99ee611
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-26 13:05:14 +0500

    question solved

---
+++ Q22.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let sum = 0 
+    for(let i = 0 ; i<nums.length ; i++){
+        sum +=nums[i]
+        let reminder = sum % k
+        if(reminder === 0){
+            return 0
+        }else{
+            return reminder
+        }
+
+};
+console.log(minOperations([3,9,7] , 5))
