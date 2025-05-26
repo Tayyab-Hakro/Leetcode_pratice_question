534ae8ab61756578c44c4994742ec749a7386c37
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-26 15:48:59 +0500

    26-5-2025

--- Q22.js
+++ Q22.js
@@ -7,12 +7,13 @@ var minOperations = function(nums, k) {
     let sum = 0 
     for(let i = 0 ; i<nums.length ; i++){
         sum +=nums[i]
+    }
         let reminder = sum % k
         if(reminder === 0){
             return 0
-        }else{
-            return reminder
         }
+         return reminder
+    }
 
-};
+
 console.log(minOperations([3,9,7] , 5))
