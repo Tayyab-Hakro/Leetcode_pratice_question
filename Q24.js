0306913b376d7f395a65aff67017430ad0ade9fb
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-27 09:35:19 +0500

    27-5-2025

---
+++ Q24.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+    console.log(diff)
+}
+console.log(arithmeticTriplets([0,1,4,6,7,10],  3))
