b3b467647bd22ef1fc049a1a10bf1da3fd960b4d
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-28 09:32:33 +0500

    working on 28-5-2025

--- Q25.js
+++ Q25.js
@@ -3,5 +3,25 @@
  * @return {boolean}
  */
 var halvesAreAlike = function(s) {
+   
+  let setVowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])  
+    let n = s.length/2
+    let count  = 0
+    let count2  = 0
+
+    for(let i = 0 ; i<n ;i++){
+        if(setVowels.has(s[i])){
+            count++
+        }
+    }
+        for(let i = n ; i<s.length ;i++){
+        if(setVowels.has(s[i])){
+            count2++
+        }
+    }
     
+   
+    return count === count2 ? true :false
 };
+console.log(halvesAreAlike("book"))
+console.log(halvesAreAlike("AbCdEfGh"))
