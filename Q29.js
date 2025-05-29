56674b4ab8cd04354dae900bfbe78d0311199089
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-29 14:21:21 +0500

    I am bored

--- Q29.js
+++ Q29.js
@@ -1,21 +1,20 @@
-/**
- * @param {number} n
- * @return {number}
- */
-var maxProduct = function(n) {
-    n  = n.toString().split("").map(Number)
-    let num = 0
-    for(let i = 0 ; i<n.length ; i++){
-          for(let j = i+1 ; j<n.length ; j++){
-            let product = n[i] * n[j]
-            if(product > num){
-                num = product
-            }
-        }
-    
+var getLucky = function(s, k) {
+    // Step 1: Convert letters to their alphabetical positions
+    let numStr = "";
+    for (let char of s) {
+        // 'a' is char code 97, so we subtract 96
+        numStr += (char.charCodeAt(0) - 96).toString();
     }
-    return num
-}; 
 
-console.log(maxProduct(31))
-console.log(maxProduct(124))
+    // Step 2: Perform digit sum transformation k times
+    while (k-- > 0) {
+        let sum = 0;
+        for (let digit of numStr) {
+            sum += parseInt(digit);
+        }
+        numStr = sum.toString();
+    }
+
+    // Return the final number
+    return parseInt(numStr);
+};
