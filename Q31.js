1bc55361b8023abebd6c74fec799ecf81daba0c4
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-05-30 16:39:35 +0500

    solving

--- Q31.js
+++ Q31.js
@@ -3,6 +3,15 @@
  * @return {number}
  */
 var minLength = function(s) {
-    console.log(s)
+
+let stack = []
+for(let i =  0 ; i<s.length ;i++){
+if(s[i] !== 'AB' || s[i] !== "CD"){
+    stack.push(s[i])
+}else{
+    stack.pop()
+}
+return stack
 };
+}
 console.log(minLength("ABFCACDB"))
