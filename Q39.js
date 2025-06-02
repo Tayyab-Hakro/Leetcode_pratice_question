74b469646c5ebbb5aa4ce12adfa961083f2a6ad0
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-06-02 16:51:14 +0500

    gpt solved

--- Q39.js
+++ Q39.js
@@ -1,18 +1,16 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var returnToBoundaryCount = function(nums) {
-    let plus = 0
-    let minut = 0
-    for(let i = 0 ; i<nums.length ;i++){
-        if(nums[i] > 0){
-            plus++
-        }else{
-            minut++
+    let position = 0;
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        position += nums[i]; // ant moves
+        console.log(position)
+        if (position === 0) {
+            count++; // ant is back at boundary
         }
-    } 
-    return  plus-minut
+    }
+
+    return count;
 };
 console.log(returnToBoundaryCount([2,3,-5]))
-console.log(returnToBoundaryCount([3,2,-3,-4]))
+// console.log(returnToBoundaryCount([3,2,-3,-4]))
