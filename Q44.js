f37c69e7de62600fe0a1145369de959e00511915
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-06-04 09:51:36 +0500

    mar

---
+++ Q44.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var countOperations = function(num1, num2) {
+const check  = num1 > num2 ? num1 - num2 : num2 - num1
+console.log(num2)
+};
+console.log(countOperations(2,  3))
