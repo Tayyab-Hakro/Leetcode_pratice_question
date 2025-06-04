1c9b72feaaa171f8806fbbcb9082070b11121c10
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-06-04 16:03:15 +0500

    4-5-2025

--- Q44.js
+++ Q44.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 var countOperations = function(num1, num2) {
-const check  = num1 > num2 ? num1 - num2 : num2 - num1
-console.log(num2)
+    let count = 0;
+    while (num1 !== 0 && num2 !== 0) {
+        if (num1 >= num2) {
+            num1 -= num2;
+        } else {
+            num2 -= num1;
+        }
+        count++;
+    }
+    return count;
 };
-console.log(countOperations(2,  3))
+
+console.log(countOperations(2, 3)); // Output: 3
