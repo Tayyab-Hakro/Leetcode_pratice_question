be34a51fe88076dc55e7501c04165f3069090974
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-06-10 21:40:33 +0500

    day completed

--- Q55.js
+++ Q55.js
@@ -1,9 +1,19 @@
-/**
- * @param {string} s
- * @return {number[]}
- */
-var diStringMatch = function(s) {
-let n = s.length
-console.log(n)
-};
+function diStringMatch(s) {
+  let low = 0;
+  let high = s.length;
+  const result = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'I') {
+      result.push(low++);
+    } else {
+      result.push(high--);
+    }
+  }
+
+  // Last number jo bacha hota hai (low == high at this point)
+  result.push(low);
+  return result;
+}
+
 console.log(diStringMatch("IDID"))
